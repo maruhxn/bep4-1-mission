d1824f1df4a90b131119556e94e0410250fc1d44
maruhxn <maruhan1016@gmail.com>
2026-09-18 17:37:17 +0900

    remove: CashFacade 불필요한 코드 제거

--- src/main/java/com/back/bounded_context/cash/app/CashFacade.java
+++ src/main/java/com/back/bounded_context/cash/app/CashFacade.java
@@ -2,21 +2,16 @@ package com.back.bounded_context.cash.app;
 
 import com.back.bounded_context.cash.domain.CashMember;
 import com.back.bounded_context.cash.domain.Wallet;
-import com.back.bounded_context.cash.out.CashMemberRepository;
-import com.back.bounded_context.cash.out.WalletRepository;
 import com.back.shared.cash.dto.CashMemberDto;
 import com.back.shared.member.dto.MemberDto;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class CashFacade {
-    private final CashMemberRepository cashMemberRepository;
-    private final WalletRepository walletRepository;
     private final CashCreateWalletUseCase cashCreateWalletUseCase;
     private final CashSyncMemberUseCase cashSyncMemberUseCase;
     private final CashSupport cashSupport;
